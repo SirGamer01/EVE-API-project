7720613ffd0af2fc2c772e294787353570ce42ef
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-06 14:25:31 +0100

    statring desing of wallet content

---
+++ ui_Elements/finance/wallet/wallet.php
@@ -0,0 +1,26 @@
+<?php
+
+function generateWallet(){
+    return <<<htmlPage
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+        box 1
+        </div>
+        <div class="col-md-4">
+        box 2
+        </div>
+        <div class="col-md-4">
+        box 3
+        </div> 
+    </div>
+
+
+
+</div>
+htmlPage;
+}
+
+generateWallet();
+
--- ui_Elements/placeHolder.php
+++ ui_Elements/placeHolder.php
@@ -1,9 +1,48 @@
 <?php
+
 echo '
 <body>
-    <div class="flex-column justify-content-center">
-        Center
-    </div>  
+
+
+<div class="container justify-content-center">
+    <h2>Place holder wallet</h2>
+
+    <div class="col justify-content-center">
+    
+        <div class="row justify-content-center">
+            <h2>Wallet</h2>
+        </div>
+        
+        <div class="row justify-content-center">
+        
+            <div class="col-md-4 justify-content-center  text-center">
+                <button class="btn-rounded btn-dark">
+                  box 1  
+                </button>
+            </div>
+            
+            <div class="col-md-4 justify-content-center text-center">
+                <button class="btn-rounded btn-dark">
+                  box 2  
+                </button>
+            </div>
+            <div class="col-md-4 justify-content-center text-center">
+            
+                <button class="btn-rounded btn-dark">
+                  box 3
+                </button>
+            </div> 
+        </div>   
+        <div class="row justify-content-center">
+            <h1 class="lady-lips-gradient">CONTENT</h1>
+        </div> 
+    </div>
+
+
+
+</div>
+
+
 </body>
 
 
