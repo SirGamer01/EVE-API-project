aae41f41b723c4d0eed82b90ac94e1e4f315aa98
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-08 22:52:06 +0100

    altaring plex and LP tabs

--- ui_Elements/placeHolder.php
+++ ui_Elements/placeHolder.php
@@ -114,51 +114,9 @@ function generatePlaceHolder(){
   
   
     <!-- Panel 2 -->
-  <div class="tab-pane fade in show" id="plex" role="tabpanel">
-    <!-- Nav tabs -->
-    <div class="row">
-      <div class="col-md-3">
-        <ul class="nav md-pills pills-primary flex-column" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" data-toggle="tab" href="#panel21" role="tab">Downloads
-              <i class="fas fa-download ml-2"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#panel22" role="tab">Orders & invoices
-              <i class="fas fa-file-alt ml-2"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#panel23" role="tab">Billing details
-              <i class="fas fa-address-card ml-2"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-9">
-        <!-- Tab panels -->
-        <div class="tab-content vertical">
-          <!-- Panel 1 -->
-          <div class="tab-pane fade in show active" id="panel21" role="tabpanel">
-            <h5 class="my-2 h5">Panel 1</h5>
-          </div>
-          <!-- Panel 1 -->
-          <!-- Panel 2 -->
-          <div class="tab-pane fade" id="panel22" role="tabpanel">
-            <h5 class="my-2 h5">Panel 2</h5>
-          </div>
-          <!-- Panel 2 -->
-          <!-- Panel 3 -->
-          <div class="tab-pane fade" id="panel23" role="tabpanel">
-            <h5 class="my-2 h5">Panel 3</h5>
-          </div>
-          <!-- Panel 3 -->
-        </div>
-      </div>
-    </div>
-    <!-- Nav tabs -->
-
+  <div class="tab-pane fade in show text-center" id="plex" role="tabpanel">
+  <br>
+   <h1>not implemented</h1>
   </div>
   <!-- Panel 2 -->
   
