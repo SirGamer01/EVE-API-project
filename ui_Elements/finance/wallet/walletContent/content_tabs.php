432d09fded21b6602d1c875b8749b460c323f9f4
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-08 22:36:09 +0100

    added tab functions

---
+++ ui_Elements/finance/wallet/walletContent/content_tabs.php
@@ -0,0 +1,30 @@
+<?php
+
+function generateWallet_Tabs(){
+    return <<<htmlPage
+        //swap content?
+
+    htmlPage;
+}
+
+
+function generateWallet_Tab1(){
+    return <<<htmlPage
+        //content?
+
+    htmlPage;
+}
+
+function generateWallet_Tab2(){
+    return <<<htmlPage
+        //content?
+
+    htmlPage;
+}
+
+function generateWallet_Tab3(){
+    return <<<htmlPage
+        //content?
+
+    htmlPage;
+}
