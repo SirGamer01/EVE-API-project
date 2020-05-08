8a4c322be4df7a662f7c419d08eefb61fd270ae0
Andrew <Mcgregor381@hotmail.co.uk>
2020-05-08 22:34:13 +0100

    added eve link

--- ui_Elements/footer.php
+++ ui_Elements/footer.php
@@ -2,7 +2,14 @@
 echo '
 <html lang="en">
     <body>
-        <footer class="page-footer font-small">
+        <footer class="page-footer font-small fixed-bottom">
+        
+            <!-- Source -->
+            <div class="footer-copyright text-center py-3">Built for
+                <a href="https://www.eveonline.com/"> Eve online</a>
+            </div>
+            <!-- Source -->     
+               
             <!-- Copyright -->
             <div class="footer-copyright text-center py-3">© 2018 Copyright:
                 <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
